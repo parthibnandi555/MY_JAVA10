cd9e749f2934c0bfcc774493641bc9f026cfa898
Parthib Nandi <nandiparthib34@gmail.com>
2026-06-26 13:55:42 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=2; i<100; i++){
+            System.out.println( a * i);
+        }
+    }
+}
